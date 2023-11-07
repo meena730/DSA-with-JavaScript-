9b83eec9f679dd076ea7125d988c31df12c604a9
Meena Dhakad <dhakadm970@gmail.com>
2023-11-07 20:11:26 +0530

    find square root

---
+++ Find-square-root.js
@@ -0,0 +1,9 @@
+const number = 81000000;
+   
+   const result = Math.sqrt(number);
+   console.log(`The square root of ${number} is ${result}`);
+   
+
+   const number1 = 1020200;
+   const result1 = Math.sqrt(number1);
+   console.log(`The square root of ${number1} is ${result1}`);
