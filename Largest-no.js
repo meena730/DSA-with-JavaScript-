13ac2f8d170eacd89c3d153398c8ce113ec5d0ba
Meena Dhakad <dhakadm970@gmail.com>
2023-11-11 19:08:27 +0530

    find a largest no. among 3

---
+++ Largest-no.js
@@ -0,0 +1,13 @@
+// find largest no among three numbers
+
+let a = 150;
+let b = 207;
+let c = 320;
+
+if (a > b && a > c) {
+  console.log(a);
+} else if (b > a && b > c) {
+  console.log(b);
+} else if (c > a && c > b) {
+  console.log(c);
+}
