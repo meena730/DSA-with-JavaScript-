58af3a6b9bd704e0286c84e138656383bb9fb202
Meena Dhakad <dhakadm970@gmail.com>
2023-11-15 22:47:39 +0530

    convert km to miles

---
+++ km-to-mile.js
@@ -0,0 +1,29 @@
+// program to convert kilometers to miles
+
+function kmToMiles(km){
+    return km * 0.621371;
+}
+// kmToMiles(23)
+console.log(kmToMiles(23));
+
+
+// 02
+function kmToMiles1(km){
+    console.log(kmToMiles(km));   
+}
+kmToMiles1(45);
+
+// 03
+
+function kmToMiles2(km){
+    return km * 0.621371;
+}
+
+console.log(kmToMiles(22));
+
+// miles to kilometers
+function milestokm(miles){
+ 
+    return miles * 1.60934;
+}
+console.log(milestokm(543));
