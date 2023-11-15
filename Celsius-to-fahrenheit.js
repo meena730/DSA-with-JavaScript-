cb51ea8ac73f7023ff903fadc1062db62313c6c4
Meena Dhakad <dhakadm970@gmail.com>
2023-11-15 22:57:46 +0530

    celsius to fahrenheit

---
+++ Celsius-to-fahrenheit.js
@@ -0,0 +1,13 @@
+// program to change celcius to fahrenheit
+
+function celciusToFahrenheit(celcius){
+    return celcius * 1.8 + 32;
+}
+console.log(celciusToFahrenheit(100));
+
+// 02
+
+function celciusToF1(celcius){
+    return celcius * 20 + 32;
+}
+console.log(celciusToF1(100));
