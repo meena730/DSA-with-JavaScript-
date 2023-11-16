26cafeb2f85a10cf0ce1516492e69d13eda2ed33
Meena Dhakad <dhakadm970@gmail.com>
2023-11-16 13:49:31 +0530

    chech the no. is positive or negative

---
+++ Number-positive-negative.js
@@ -0,0 +1,28 @@
+
+// program to find no. is positive or negative numbers
+// ----------------------------------------------------------------
+
+function checkNumber(number){
+    // let number = parseInt(prompt('Enter a number'));
+    if (number > 0) {
+        console.log(number +'is positive');
+    } else if (number < 0) {
+        console.log(number +'is negative');
+    } else {
+               console.log(number +'is zero');
+    }
+}checkNumber(100 );
+
+function checkNumber2(number1){
+    if(number1<0){
+        return 'Given number is negative'
+    } else if(number1>0){
+        return 'Given number is positive'
+    }
+    else{
+        return 'Given number is zero'
+    }
+}
+console.log(checkNumber2(-110));
+
+    
