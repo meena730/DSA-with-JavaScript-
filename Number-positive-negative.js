323ba38bf95600f41bf41ce8034cd6903afc0b14
Meena Dhakad <dhakadm970@gmail.com>
2023-11-16 14:17:57 +0530

    check the no. is positive/negtive /zero

--- Number-positive-negative.js
+++ Number-positive-negative.js
@@ -3,7 +3,7 @@
 // ----------------------------------------------------------------
 
 function checkNumber(number){
-    // let number = parseInt(prompt('Enter a number'));
+
     if (number > 0) {
         console.log(number +'is positive');
     } else if (number < 0) {
