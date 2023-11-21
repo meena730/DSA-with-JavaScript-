cbd67c6d9d8a186e9f9f6f07f5de1ea212e1b1cd
Meena Dhakad <dhakadm970@gmail.com>
2023-11-21 20:31:04 +0530

    Multiplication table

---
+++ Table.js
@@ -0,0 +1,15 @@
+// create a Multiplication  table of numbers
+function MultiplicationTable(){
+    for(var i=5;i<=50;i+=5){
+        {
+            console.log(i)
+        }
+    }
+}
+
+MultiplicationTable()
+
+
+for(var i= 5;i<=50; i+=5){
+    console.log(i)
+}
