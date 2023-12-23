a77c71f81c7596ebe4aa13191afef6a984c1284a
Meena Dhakad <dhakadm970@gmail.com>
2023-12-23 17:29:04 +0530

    check last digit

--- check-last-digit.js
+++ check-last-digit.js
@@ -1,32 +1,30 @@
 // Method 1: Using Modulo Operator
 function SameLastDigitMethod1(num1, num2) {
-    return num1 % 10 === num2 % 10;
+  return num1 % 10 === num2 % 10;
 }
 
-// Method 2: Using toString() method
+// // Method 2: Using toString() method
 function SameLastDigitMethod2(num1, num2) {
-    return num1.toString().slice(-1) === num2.toString().slice(-1);
+  return num1.toString().slice(-1) === num2.toString().slice(-1);
 }
 
-let number1 = 126;
-let number2 = 456;
+let number1 = 1276;
+let number2 = 4556;
 
 console.log("Method 1:", SameLastDigitMethod1(number1, number2));
 
 console.log("Method 2:", SameLastDigitMethod2(number1, number2));
 
-
-
 // 02
 
 // Method 1: Using Modulo Operator
 function SameLastDigitMethod1(num1, num2) {
-    return num1 % 10 === num2 % 10;
+  return num1 % 10 === num2 % 10;
 }
 
 // Method 2: Using toString() method
 function SameLastDigitMethod2(num1, num2) {
-    return num1.toString().slice(-1) === num2.toString().slice(-1);
+  return num1.toString().slice(-1) === num2.toString().slice(-1);
 }
 
 let number3 = 155;
