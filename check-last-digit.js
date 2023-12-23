b7a1cb0cbd52a85f641feb9be55d30f090160ccc
Meena Dhakad <dhakadm970@gmail.com>
2023-12-23 17:20:56 +0530

    to check last digit same or not

---
+++ check-last-digit.js
@@ -0,0 +1,37 @@
+// Method 1: Using Modulo Operator
+function SameLastDigitMethod1(num1, num2) {
+    return num1 % 10 === num2 % 10;
+}
+
+// Method 2: Using toString() method
+function SameLastDigitMethod2(num1, num2) {
+    return num1.toString().slice(-1) === num2.toString().slice(-1);
+}
+
+let number1 = 126;
+let number2 = 456;
+
+console.log("Method 1:", SameLastDigitMethod1(number1, number2));
+
+console.log("Method 2:", SameLastDigitMethod2(number1, number2));
+
+
+
+// 02
+
+// Method 1: Using Modulo Operator
+function SameLastDigitMethod1(num1, num2) {
+    return num1 % 10 === num2 % 10;
+}
+
+// Method 2: Using toString() method
+function SameLastDigitMethod2(num1, num2) {
+    return num1.toString().slice(-1) === num2.toString().slice(-1);
+}
+
+let number3 = 155;
+let number4 = 567;
+
+console.log("Method 1:", SameLastDigitMethod1(number3, number4));
+
+console.log("Method 2:", SameLastDigitMethod2(number3, number4));
